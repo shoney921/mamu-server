9cfc7f9899f8cf5dd051bb543b9aad544f947ac3
shoney <ddmczp@naver.com>
2023-05-02 23:53:48 +0900

    [feat] User Mapper, User Entity 생성

---
+++ src/main/java/com/example/mamuserver/entity/User.java
@@ -0,0 +1,7 @@
+package com.example.mamuserver.entity;
+
+public class User {
+    private String id;
+    private String name;
+    private Integer age;
+}
---
+++ src/main/java/com/example/mamuserver/mapper/user/UserMapper.java
@@ -0,0 +1,11 @@
+package com.example.mamuserver.mapper.user;
+
+import com.example.mamuserver.entity.User;
+
+public interface UserMapper {
+
+    void save(User user);
+
+//    void update(@Param("id") Long id, @Param("updateParam") UserUpdateDto)
+
+}
